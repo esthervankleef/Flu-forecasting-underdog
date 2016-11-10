fac5ab7ceef7c8c0a9c9ce924b763203512c8917
Rene <rene.niehus@gmx.de>
2016-11-10 08:58:23 +0700

    Add numbers to the scripts that have to be run in succession for new forecast

--- 3_data_manip_2.R
+++ 3_data_manip_2.R
@@ -1,6 +1,6 @@
 ###### Flu Forecasting Challenge ####### 
 
-## title: "Data Manipulation 2"
+## title: "Data Manipulation 2: where we add covariates"
 ## author: "Underdog"
 ## date: "20 October 2016"
 
@@ -103,4 +103,4 @@ DF2 <- DF1[week_pos:end.timeline,]
 #### save
 DF <- DF2
 savename <- paste0("./Data/", script_name,".Rda")
-save(DF, file = savename)
+save(DF,most_current_week, file = savename)
