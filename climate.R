497c5bc41b39f4fa36541d5aca7aa08186e406c4
esthervankleef <esther.vankleef@lshtm.ac.uk>
2016-12-12 12:58:55 +0100

    Automate climate data extraction

--- climate.R
+++ climate.R
@@ -4,19 +4,20 @@
 ## title: "Forecasting Flu Template"
 ## author: "Underdog"
 ## date: "19 October 2016"
-
-
 rm(list=ls())
 script_name <- "clim_data"
-# Downloaded from: http://www.ncdc.noaa.gov/temp-and-precip/us-weekly/
-# Click on excel logo. Then I copy pasted the text file into excel, saved it as a text and then imported
-# this text file in a new spread sheet and saved it as .csv
 
-# In this new spread sheet, I have manually removed the first row (saying temp in F etc), and the " from
-# first and last column. Then saved this as weekly_temp_raw.csv
+library(data.table); library(dplyr)
 
 # Load data
-clim = read.csv("./Data/Weekly_temp_raw47.csv", colClasses="character")
+clim <- data.frame(fread('https://www.ncdc.noaa.gov/temp-and-precip/us-weekly/wkly.csv'))
+
+forecast_week = length(clim$weekEnding)-length(c((1:52),c(1:53),c(1:52)))
+
+# Add week number, year 2014 has 53 weeks
+clim$week = c(c(1:52),c(1:53),c(1:52),c(1:forecast_week)) 
+clim$week = ifelse(clim$week %in%c(1:9), paste0("0",clim$week), clim$week) # adds 0 to single numbers
+clim$year = c(rep("2013",52),rep("2014",53),rep("2015",52),rep("2016",forecast_week)) # idem
 
 # Add week number, year 2014 has 53 weeks
 clim$week = c(c(1:52),c(1:53),c(1:52),c(1:47)) # When loading in new data, change the last figure to the current week
@@ -34,7 +35,6 @@ for(i in c(2:10)){
 }
 
 # Temperature patterns seems quite similar across the regions
-
 
 #  Plot Temperature anomalies
 plot(clim$natAnom, main="Temperature anomaly", ylim=c(-15,15))
